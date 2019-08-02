2455ea5e2b82d4afb48cd34457b77f49baed469c
Rahul Raj <rahulraj@Rahuls-MacBook-Pro.local>
2019-08-02 12:54:57 +0530

    Updates

---
+++ precache-manifest.2928e7eb1a460ef5251a9700d8a0bbc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b7627cfe20076cbe1ecd5711d615cb7",
+    "url": "/Simple_Is_Best/index.html"
+  },
+  {
+    "revision": "2c70ca151b96bbe1383e",
+    "url": "/Simple_Is_Best/static/css/main.bc8d1910.chunk.css"
+  },
+  {
+    "revision": "3cdd65d12709bf46b0f4",
+    "url": "/Simple_Is_Best/static/js/2.2720b7f1.chunk.js"
+  },
+  {
+    "revision": "2c70ca151b96bbe1383e",
+    "url": "/Simple_Is_Best/static/js/main.c28f81d5.chunk.js"
+  },
+  {
+    "revision": "025f2a2ef7e740ae0fc2",
+    "url": "/Simple_Is_Best/static/js/runtime~main.fe24ad13.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Simple_Is_Best/precache-manifest.2928e7eb1a460ef5251a9700d8a0bbc8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Simple_Is_Best/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
