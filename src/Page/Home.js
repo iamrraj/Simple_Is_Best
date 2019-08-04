6e2577a394a9648ec42ba02650eb276a6e56651c
Rahul Raj <rahulraj@Rahuls-MacBook-Pro.local>
2019-08-04 11:10:50 +0530

    Added New Api

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import Sidebar from '../Privacy/Sidebar';
 import './home.css';
 // import Service from './Service'
-import Moment from 'react-moment';
+// import Moment from 'react-moment';
 
 
 class Home extends Component {
@@ -31,14 +31,15 @@ class Home extends Component {
         return (
         <div>
             <section id="fact23">
-                <div class="container text-center">
-                <div class="home-search ">
-                    <div class="overlay">
-                    <h1 class="display-4 mb-4 text-white">
-                    <h1 style={{color: "white", fontSize:"53px"}}><span style={{color: "orange"}}>SI</span>
+                <div className="container text-center">
+                <div className="home-search ">
+                    <div className="overlay">
+                    <div className="display-4 mb-4 text-white">
+                    <h1 style={{color: "white", fontSize:"53px"}}>
+                        <span style={{color: "orange"}}>SI</span>
                      MP<span style={{color: "orange"}} >LE</span> IS <span style={{color: "orange"}} >B</span>ES<span style={{color: "orange"}}>T</span> </h1>
-                    <h4 class="text-white">Stories about Python,React Js, Django, Node Js and Web Development</h4>
-                    </h1>
+                    <h4 className="text-white">Stories about Python,React Js, Django, Node Js and Web Development</h4>
+                    </div>
                     </div>
                 </div>
                 </div>
@@ -56,8 +57,8 @@ class Home extends Component {
 
                                 <center><h1 style={{textalign:"center"}} className="text-dark" > { post.title } </h1></center>
                                                 <br></br>
-                                <p style={{textAlign:"center", opacity: "0.7"}} className="text-dark"><i className="fa fa-calendar"></i> { post.publish } &nbsp; <i class="fa fa-user"></i>  { post.author }  &nbsp;
-                                <i className="fa fa-clock-o"></i>  { post.read_time}  Minutes Read    &nbsp; <i class="fa fa-comments"></i>  <a href="{{post.slug}}#disqus_thread" className="text-dark">Comments</a>   &nbsp;   
+                                <p style={{textAlign:"center", opacity: "0.7"}} className="text-dark"><i className="fa fa-calendar"></i> { post.publish } &nbsp; <i className="fa fa-user"></i>  { post.author }  &nbsp;
+                                <i className="fa fa-clock-o"></i>  { post.read_time}  Minutes Read    &nbsp; <i className="fa fa-comments"></i>  <span className="text-dark"> Comments </span>   &nbsp;   
                                 <i className="fa fa-eye"></i> Views </p >
                                 <img id="img"  src={ post.murl} alt={ post.title }  ></img>
                                                 <br></br>
@@ -73,12 +74,12 @@ class Home extends Component {
                         <p >
             
                         
-                        <button class="btn " style={{background:"orange", color:"whit"}}> { post.name }</button>
+                        <button className="btn " style={{background:"orange", color:"whit"}}> { post.name }</button>
                             
                             <span style={{float:"right"}}>
-                                <a href="http://www.facebook.com/sharer.php?u={{request.build_absolute_uri}}" onclick="window.open('http://www.facebook.com/sharer.php?u={{request.build_absolute_uri}}/','Facebook share', 'height=600, width=800')" target="_blank" class="fa fa-facebook text-center text-white" style={{textDecoration:"none",backgroundColor:"orange",padding:"12px",borderRadius:"50%", width:"45px", fontSize:"22px",marginRight:"7px"}} > </a>
-                                <a href="https://twitter.com/share?url={{request.build_absolute_uri}}&amp;text=Simple%20Is%20Best&amp;hashtags=simpleisbest" target="_blank" class="fa fa-twitter text-center text-white" style={{textDecoration:"none",backgroundColor:"orange",padding:"12px",borderRadius:"50%", width:"45px", fontSize:"22px",marginRight:"7px"}}> </a>
-                                <a href="http://www.linkedin.com/shareArticle?mini=true&amp;url={{request.build_absolute_uri}}" target="_blank" class="fa fa-linkedin text-center text-white" style={{textDecoration:"none",backgroundColor:"orange",padding:"12px",borderRadius:"50%", width:"45px", fontSize:"22px",marginRight:"7px"}}> </a>
+                                <a href="http://www.facebook.com/sharer.php?u={{request.build_absolute_uri}}"  target="_blank" rel="noopener noreferrer" className="fa fa-facebook text-center text-white" style={{textDecoration:"none",backgroundColor:"orange",padding:"12px",borderRadius:"50%", width:"45px", fontSize:"22px",marginRight:"7px"}} > </a>
+                                <a href="https://twitter.com/share?url={{request.build_absolute_uri}}&amp;text=Simple%20Is%20Best&amp;hashtags=simpleisbest" target="_blank"  rel="noopener noreferrer" className="fa fa-twitter text-center text-white" style={{textDecoration:"none",backgroundColor:"orange",padding:"12px",borderRadius:"50%", width:"45px", fontSize:"22px",marginRight:"7px"}}> </a>
+                                <a href="http://www.linkedin.com/shareArticle?mini=true&amp;url={{request.build_absolute_uri}}" target="_blank" rel="noopener noreferrer"  className="fa fa-linkedin text-center text-white" style={{textDecoration:"none",backgroundColor:"orange",padding:"12px",borderRadius:"50%", width:"45px", fontSize:"22px",marginRight:"7px"}}> </a>
                                 
 
                             </span>
